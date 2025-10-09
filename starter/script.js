4a3214223b9c87e206a7e5362c1298983e736c18
Akinolu <ainaakinolu@gmail.com>
2025-10-09 15:11:56 +0100

    Lesson 32.

---
+++ starter/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let hasDriversLicence = false;
+const passTest = true;
+
+if (passTest) hasDriversLicence = true;
+if (hasDriversLicence) console.log('I can drive')
+
+// console.log("My name is Khan and I am not a good boy!")
+
+
+const interface = 'Audio';
+const private = 534;
