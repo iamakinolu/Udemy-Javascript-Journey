373089d684762c84ded077d83be1e5a33d2d92d6
Akinolu <ainaakinolu@gmail.com>
2025-11-15 17:59:36 +0100

    Functions

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,3 @@
-
 // let hasDriversLicence = false;
 // const passTest = true;
 
@@ -7,15 +6,29 @@
 
 // // console.log("My name is Khan and I am not a good boy!")
 
-
 // const interface = 'Audio';
 // const private = 534;
 
-function logger(){
-    console.log('My name is Jonas');
+function logger() {
+  console.log("My name is Jonas");
 }
 
 //calling / running / invoking function
 logger();
 logger();
-logger();  
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+//Arguments (fruitProcessor(5, 0))
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const apppleOrangeJuice = fruitProcessor(2, 4);
+console.log(apppleOrangeJuice);
+
+const num = Number('23');
+console.log(num);
