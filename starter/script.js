c65539412ad17b2a796f1d111a3121b2b24d2e5d
Akinolu <ainaakinolu@gmail.com>
2025-11-15 17:54:52 +0100

    Functions

--- starter/script.js
+++ starter/script.js
@@ -1,13 +1,21 @@
-'use strict';
 
-let hasDriversLicence = false;
-const passTest = true;
+// let hasDriversLicence = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicence = true;
-if (hasDriversLicence) console.log('I can drive')
+// if (passTest) hasDriversLicence = true;
+// if (hasDriversLicence) console.log('I can drive')
 
-// console.log("My name is Khan and I am not a good boy!")
+// // console.log("My name is Khan and I am not a good boy!")
 
 
-const interface = 'Audio';
-const private = 534;
+// const interface = 'Audio';
+// const private = 534;
+
+function logger(){
+    console.log('My name is Jonas');
+}
+
+//calling / running / invoking function
+logger();
+logger();
+logger();  
